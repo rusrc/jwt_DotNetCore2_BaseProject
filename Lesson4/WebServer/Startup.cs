9d2debdc5a0772194e6e05f4debd1964610c2e38
R. Scher <shcherbakov@reksoft.ru>
2018-01-25 18:43:35 +0300

    Add lessons folders

--- Lesson4/WebServer/Startup.cs
+++ Lesson4/WebServer/Startup.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Identity;
 using WebServer.Models;
 using Microsoft.EntityFrameworkCore;
@@ -106,7 +106,6 @@ namespace WebServer
                     });
 
             services.AddMvc();
-
 
         }
 
